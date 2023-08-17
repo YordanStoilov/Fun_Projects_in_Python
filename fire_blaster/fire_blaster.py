2ff4b5ee5de18dc2e9013f232b6ed0b0f27e2059
YordanStoilov <iordan27@gmail.com>
2023-08-17 17:57:16 +0300

    Adding shooting bullets feature

--- fire_blaster/fire_blaster.py
+++ fire_blaster/fire_blaster.py
@@ -7,12 +7,19 @@ pygame.display.set_caption("Fire Blaster")
 
 LIGHT_GREEN = (213, 255, 208)
 BLACK = (0, 0, 0)
+RED = (194, 51, 115)
+YELLOW = (247, 233, 135)
 
-BORDER = pygame.Rect(WIDTH / 2 - 5, 0, 10, HEIGHT)
+BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
 FPS = 60
 SHIP_SIZE = (70, 50)
 MOVEMENT_SPEED = 5
+BULLET_SPEED = 7
+MAX_BULLETS = 5
+
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
 
 YELLOW_SPACESHIP_IMG = pygame.image.load(
     os.path.join("Assets", "spaceship_yellow.png"))
@@ -23,12 +30,18 @@ RED_SPACESHIP_IMG = pygame.image.load(
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMG, SHIP_SIZE), 270)
 
 
-def draw_window(red, yellow):
+def draw_window(red, yellow, red_bull, yellow_bull):
     WIN.fill(LIGHT_GREEN)
     pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
+
+    for bullet in red_bull:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in yellow_bull:
+        pygame.draw.rect(WIN, YELLOW, bullet)
 
 
 def movement_controls_yellow(keys, yellow_ship, speed):
@@ -53,9 +66,26 @@ def movement_controls_red(keys, red_ship, speed):
         red_ship.y += speed
 
 
+def handle_bullets(yellow_player, red_player, yellow_bull, red_bull):
+    for bullet in yellow_bull:
+        bullet.x += BULLET_SPEED
+        if red_player.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bull.remove(bullet)
+
+    for bullet in red_bull:
+        bullet.x -= BULLET_SPEED
+        if yellow_player.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bull.remove(bullet)
+
+
 def main():
     red = pygame.Rect(750, 200, WIDTH, HEIGHT)
     yellow = pygame.Rect(100, 200, WIDTH, HEIGHT)
+
+    yellow_bullets = []
+    red_bullets = []
 
     clock = pygame.time.Clock()
     run = True
@@ -65,11 +95,21 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_v and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height // 2 - 2, 10, 5)
+                    yellow_bullets.append(bullet)
+                if event.key == pygame.K_b and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(red.x, red.y + red.height // 2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+
         keys_pressed = pygame.key.get_pressed()
         movement_controls_yellow(keys_pressed, yellow, MOVEMENT_SPEED)
         movement_controls_red(keys_pressed, red, MOVEMENT_SPEED)
 
-        draw_window(red, yellow)
+        handle_bullets(yellow, red, yellow_bullets, red_bullets, )
+
+        draw_window(red, yellow, red_bullets, yellow_bullets)
     pygame.quit()
 
 
