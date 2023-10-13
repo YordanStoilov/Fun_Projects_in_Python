78b116e5d19188f655b9e217cd0c59999cc73f8b
YordanStoilov <iordan27@gmail.com>
2023-10-13 19:46:14 +0300

    fixed some minor errors

--- Healthcare/main.py
+++ Healthcare/main.py
@@ -9,7 +9,7 @@ current_user_info = {"Name": "", "Age": 0, "Weight": 0, "Height": 0, "Blood Pres
 def get_user_vitals():
     user_vitals = HealthcareApp(current_user_info["Name"], current_user_info["Age"], current_user_info["Weight"],
                                 current_user_info["Height"], tuple(current_user_info["Blood Pressure"]))
-    user_text = f"{user_vitals.get_bmi()}\n{user_vitals.get_blood_pressure_status()}\n{user_vitals.get_max_heartrate()}" \
+    user_text = f"{user_vitals.get_bmi()}\n{user_vitals.get_blood_pressure_status()}\n"\
                 f"{user_vitals.get_max_heartrate()}\n{user_vitals.get_goal_pulse()}"
     return user_text
 
@@ -52,6 +52,12 @@ def store_blood_pressure():
 
 
 def get_ready():
+    store_name_info()
+    store_age_info()
+    store_height_info()
+    store_weight_info()
+    store_blood_pressure()
+
     for key, value in current_user_info.items():
         if not value or value == 0:
             print(value)
@@ -81,68 +87,43 @@ root = Tk()
 root.geometry("750x650")
 root.title("HealthcareApp")
 
-# Creating
-#
-# Name Box:
+# Creating Name Box:
 name_label = Label(root, text="Enter your name")
 name_bar = Entry(root, width=60, borderwidth=8)
 name_label.grid(row=0, column=0)
 name_bar.grid(row=1, column=0)
 
-# Name Button:
-button_name = Button(root, text="Store", command=store_name_info)
-button_name.grid(row=1, column=9)
 
-# Creating
-#
-# Age Box:
+# Creating Age Box:
 age_label = Label(root, text="Enter your age")
 age_bar = Entry(root, width=60, borderwidth=8)
 age_label.grid(row=2, column=0)
 age_bar.grid(row=3, column=0)
 
-# Age Button:
-button_age = Button(root, text="Store", command=store_age_info)
-button_age.grid(row=3, column=9)
-
-# Creating
-#
-# Height Box:
+# Creating Height Box:
 height_label = Label(root, text="Enter your height")
 height_bar = Entry(root, width=60, borderwidth=8)
 height_label.grid(row=4, column=0)
 height_bar.grid(row=5, column=0)
 
-# Height Button:
 
-button_height = Button(root, text="Store", command=store_height_info)
-button_height.grid(row=5, column=9)
-
-# Creating
-#
-# Weight Box:
+# Creating Weight Box:
 weight_label = Label(root, text="Enter your weight")
 weight_bar = Entry(root, width=60, borderwidth=8)
 weight_label.grid(row=6, column=0)
 weight_bar.grid(row=7, column=0)
 
-# Weight Button:
-button_height = Button(root, text="Store", command=store_weight_info)
-button_height.grid(row=7, column=9)
 
-# Creating
-# Blood Pressure Box
+# Creating Blood Pressure Box
 bp_label = Label(root, text="Enter your blood pressure separated by space")
 bp_bar = Entry(root, width=60, borderwidth=8)
 bp_label.grid(row=8, column=0)
 bp_bar.grid(row=9, column=0)
 # Blood Pressure Button:
 
-button_bp = Button(root, text="Store", command=store_blood_pressure)
-button_bp.grid(row=9, column=9)
-
 # Button Ready:
 button_ready = Button(root, text="Ready", command=get_ready)
-button_ready.grid(row=12, column=8)
+button_ready.config(width=15, height=2, borderwidth=8)
+button_ready.grid(row=10, column=3)
 
 root.mainloop()
